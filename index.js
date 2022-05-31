8062a937550e0b31ca717542bb347f1907d726b6
Diego <ctt.andrade94@gmail.com>
2022-05-31 11:20:34 -0300

    Last day of Project

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
 (() => {
+    const cartItems = [];
     const cartButton = document.querySelectorAll(".cart-icon");
     const closeCartButton = document.querySelector("span.close-icon");
+    const elementsToHide = document.querySelectorAll("[data-empty-cart]");
+
+    if (cartItems.length === 0) {
+        
+        elementsToHide.forEach((element) => {
+            element.style.display = "block";
+        });
+
+    } else {
+        elementsToHide.forEach((element) => {
+            element.style.display = "none";
+        });
+    }
 
     closeCartButton.onclick = () => {
         const cart = document.querySelector(".cart");
@@ -9,6 +23,8 @@
     cart.style.display = "none";
     }
 
+
+    // When clicking in the Cart Button, opens the Cart.
     cartButton.forEach(element => {
         element.addEventListener("click", () => {
             const cart = document.querySelector(".cart");
@@ -17,8 +33,6 @@
         })
     })
     
-
-    // When clicking in the Cart Button, opens the Cart.
 
     // When clicking in closeCartButton, hide the Cart.
     console.log(cartButton);
